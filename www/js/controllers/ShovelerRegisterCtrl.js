9073739805f8bbfc073954c2856eaaf54fc33090
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-13 13:50:44 -0500

    creating shoveler signup functionality

--- www/js/controllers/ShovelerRegisterCtrl.js
+++ www/js/controllers/ShovelerRegisterCtrl.js
@@ -1,5 +1,11 @@
 angular.module('shoveler.controllers')
 
-.controller('ShovelerRegisterCtrl',['$scope',function($scope) {
+.controller('ShovelerRegisterCtrl',['$scope','Shoveler',function($scope,Shoveler) {
+  $scope.shoveler = {};
+  $scope.submitForm = function() {
+    $scope.shoveler.name = $scope.shoveler.firstName + " " + $scope.shoveler.lastName;
+    Shoveler.signup($scope.shoveler);
+    console.log($scope.shoveler);
+  };
 
 }]);
