03d4744386da37a46e4f1ee98f0cbeb4d8ea8872
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-13 13:41:23 -0500

    building post job functionality

--- www/js/controllers/OwnerPostJobCtrl.js
+++ www/js/controllers/OwnerPostJobCtrl.js
@@ -1,5 +1,11 @@
 angular.module('shoveler.controllers')
 
-.controller('OwnerPostJobCtrl',['$scope',function($scope) {
+.controller('OwnerPostJobCtrl',['$scope','Owner','Job',function($scope, Owner, Job) {
+  $scope.job = {};
 
+  $scope.submitForm = function() {
+    $scope.job.owner_id = Owner.getId();
+    console.log($scope.job);
+    Job.post($scope.job);
+  };
 }]);
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -11,7 +11,20 @@ angular.module('shoveler.services', [])
         });
       },
       getId: function() {
-        return id; 
+        return id;
+      }
+    };
+  }
+])
+
+.factory('Job',['$http','API_BASE',
+  function($http,API_BASE) {
+    return {
+      post: function(job) {
+        $http.post(API_BASE + 'jobs/', job).then(function(response){
+          console.log(response);
+          // id = response.data.owner._id;
+        });
       }
     };
   }
