3a95e3655bcf8248c9c637d3327937902ce87bf8
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-13 13:24:51 -0500

    linking register form to API

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('shoveler', ['ionic', 'shoveler.controllers', 'ngMap', 'ngCordova'])
+angular.module('shoveler', ['ionic', 'shoveler.controllers', 'shoveler.services', 'ngMap', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -58,4 +58,6 @@ angular.module('shoveler', ['ionic', 'shoveler.controllers', 'ngMap', 'ngCordova
 
   $urlRouterProvider.otherwise('/home');
 
-});
+})
+
+.constant('API_BASE','http://localhost:3210/api/');
--- www/js/controllers/OwnerRegisterCtrl.js
+++ www/js/controllers/OwnerRegisterCtrl.js
@@ -1,12 +1,13 @@
 angular.module('shoveler.controllers')
 
-.controller('OwnerRegisterCtrl',['$scope',function($scope) {
+.controller('OwnerRegisterCtrl',['$scope','Owner',function($scope, Owner) {
   $scope.owner = {};
   $scope.submitForm = function(form) {
     $scope.owner.address= $scope.owner.street + ", " +
                           $scope.owner.city + ", " +
                           $scope.owner.state;
     $scope.owner.name = $scope.owner.firstName + " " + $scope.owner.lastName;
+    Owner.signup($scope.owner);
     console.log($scope.owner);
   };
 }]);
