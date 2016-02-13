a5701152b5bf97db43a21d3b81f72d8ea08ab267
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-13 16:44:35 -0500

    adding custom map markers and current location marker and slidebox

--- www/js/controllers/ShovelerMapCtrl.js
+++ www/js/controllers/ShovelerMapCtrl.js
@@ -1,31 +1,46 @@
 angular.module('shoveler.controllers')
 
-.controller('ShovelerMapCtrl',['$scope','NgMap','Job',function($scope,NgMap,Job) {
+.controller('ShovelerMapCtrl',['$scope','NgMap','Job','$ionicSlideBoxDelegate',
+  function($scope,NgMap,Job,$ionicSlideBoxDelegate) {
 
-  var map;
-  // console.log(jobs);
+    var map;
 
-  getJobs()
-  .then(getMap);
+    getJobs()
+    .then(getMap);
 
 
-  function getJobs() {
-    return Job.get();
+    function getJobs() {
+      return Job.get().then(function(jobs){
+        console.log(jobs);
+        $scope.jobs = jobs;
+        $ionicSlideBoxDelegate.update();
+        return jobs;
+      });
+    }
+
+    function getMap() {
+      return NgMap.getMap().then(function(theMap){
+        map = theMap;
+        for (var i = 0; i < $scope.jobs.length; i++) {
+          var icon = {
+            url: 'img/shoveler_marker@2x.png',
+            scaledSize: {width: 20, height: 28}
+          };
+
+          if (i === 0) {
+            icon.url = 'img/shoveler_marker_selected@2x.png';
+          }
+
+          $scope.jobs[i].marker = new google.maps.Marker({
+           position: {
+             lat: $scope.jobs[i].owner.coords.lat,
+             lng: $scope.jobs[i].owner.coords.lng,
+           },
+           map: map,
+           icon: icon
+         });
+        }
+      });
+    }
   }
-
-  function getMap(jobs) {
-    return NgMap.getMap().then(function(theMap){
-      map = theMap;
-      for (var i = 0; i < jobs.length; i++) {
-
-        var marker = new google.maps.Marker({
-         position: {
-           lat: jobs[i].owner.coords.lat,
-           lng: jobs[i].owner.coords.lng
-         },
-         map: map
-       });
-      }
-    });
-  }
-}]);
+]);
