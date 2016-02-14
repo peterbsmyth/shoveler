86e6824b339f4aad2c7662777414ca69b635a196
Peter B Smith <peterbutlersmith@gmail.com>
2016-02-14 13:01:56 -0500

    adding select marker functions

--- www/js/controllers/ShovelerMapCtrl.js
+++ www/js/controllers/ShovelerMapCtrl.js
@@ -1,14 +1,16 @@
 angular.module('shoveler.controllers')
 
-.controller('ShovelerMapCtrl',['$scope','NgMap','Job','$ionicSlideBoxDelegate',
-  function($scope,NgMap,Job,$ionicSlideBoxDelegate) {
+.controller('ShovelerMapCtrl',['$scope','$location','NgMap','Job','$ionicSlideBoxDelegate','$ionicLoading',
+  function($scope,$location,NgMap,Job,$ionicSlideBoxDelegate,$ionicLoading) {
 
     var map;
-
+    showLoading();
     getJobs()
     .then(getMap);
 
-
+    $scope.goTo = function(job) {
+      $location.url('shoveler/job_details/' + job._id);
+    };
     function getJobs() {
       return Job.get().then(function(jobs){
         console.log(jobs);
@@ -17,6 +19,22 @@ angular.module('shoveler.controllers')
         return jobs;
       });
     }
+
+    $scope.updateSlide = function(index) {
+      console.log(index);
+      for (var i = 0; i < $scope.jobs.length; i++) {
+        var icon = {
+          url: 'img/shoveler_marker@2x.png',
+          scaledSize: {width: 20, height: 28}
+        };
+
+        if (i === index) {
+          icon.url = 'img/shoveler_marker_selected@2x.png';
+        }
+
+        $scope.jobs[i].marker.setIcon(icon);
+      }
+    };
 
     function getMap() {
       return NgMap.getMap().then(function(theMap){
@@ -30,7 +48,7 @@ angular.module('shoveler.controllers')
           if (i === 0) {
             icon.url = 'img/shoveler_marker_selected@2x.png';
           }
-
+          $scope.jobs[i].index = i;
           $scope.jobs[i].marker = new google.maps.Marker({
            position: {
              lat: $scope.jobs[i].owner.coords.lat,
@@ -39,8 +57,41 @@ angular.module('shoveler.controllers')
            map: map,
            icon: icon
          });
+
+         addMarkerClick($scope.jobs[i]);
+
+
         }
+        $scope.j = $scope.jobs[0];
+        hideLoading();
       });
+    }
+
+    function addMarkerClick(job) {
+      job.marker.addListener('click',function() {
+        updateSelected(job);
+        $ionicSlideBoxDelegate.slide(job.index);
+      });
+    }
+
+    function updateSelected(job) {
+      $scope.j.marker.setIcon({
+        url: 'img/shoveler_marker@2x.png',
+        scaledSize: {width: 20, height: 28}
+      });
+      job.marker.setIcon({
+        url: 'img/shoveler_marker_selected@2x.png',
+        scaledSize: {width: 20, height: 28}
+      });
+      $scope.j = job;
+    }
+
+    function showLoading() {
+      $ionicLoading.show({});
+    }
+
+    function hideLoading() {
+      $ionicLoading.hide();
     }
   }
 ]);
